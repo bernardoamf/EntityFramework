aa33ec34e4c6e49458695bac0fe556e6e9c096ae
Bernardo A. Munoz <bernardoamf@hotmail.com>
2015-07-10 22:54:44 -0700

    with Enums

---
+++ AdventureWorksModel/AdventureWorksModel/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureWorksModel
+{
+    namespace AdventureWorksModel
+    {
+        public enum ShipMethod
+        {
+            Land = 1,
+            Sea = 2,
+            Air = 3,
+            Transporter = 4
+        }
+    }
+}
